15e523d96be6cb4c117cf2379d96d86a7dac1299
juan18506 <juan185067@gmail.com>
2023-09-23 20:18:43 -0300

    updated thunks HTTP request using axios

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,12 +1,15 @@
+import { pokemonApi } from '../../../api/pokemonApi';
 import { setPokemons, startLoadingPokemons } from './pokemonSlice';
 
 export const getPokemons = ( page = 0 ) => {
   return async ( dispatch, getState ) => {
     dispatch( startLoadingPokemons() );
 
-    const res = await fetch(`https:pokeapi.co/api/v2/pokemon?limit=10&offset=${ page * 10 }`);
-    const data = await res.json();
-    console.log(data);
+    // const res = await fetch(`https:pokeapi.co/api/v2/pokemon?limit=10&offset=${ page * 10 }`);
+    // const data = await res.json();
+    // console.log(data);
+
+    const { data } = await pokemonApi.get(`/pokemon?limit=10&offset=${ page * 10 }`);
 
     dispatch( setPokemons({
       pokemons: data.results,
