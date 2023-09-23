1d85886a2cb895391474c9ddd69698a76e53a64d
juan18506 <juan185067@gmail.com>
2023-09-23 20:13:45 -0300

    updated getPokemons  using fetch API

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -5,6 +5,13 @@ export const getPokemons = ( page = 0 ) => {
     dispatch( startLoadingPokemons() );
 
     //TODO: realizar peticion HTTP
-    // dispatch( setPokemons() );
+    const res = await fetch(`https:pokeapi.co/api/v2/pokemon?limit=10&offset=${ page * 10 }`);
+    const data = await res.json();
+    console.log(data);
+
+    dispatch( setPokemons({
+      pokemons: data.results,
+      page: page + 1,
+    }) );
   };
 };
