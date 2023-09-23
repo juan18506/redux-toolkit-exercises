de2551a254e54daaba6463c8b375c7148c9525fa
juan18506 <juan185067@gmail.com>
2023-09-23 20:48:56 -0300

    created RTK Query configs

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './index.css';
 import { store } from './store';
-import { PokemonApp } from './PokemonApp.jsx';
+import { TodoApp } from './TodoApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={ store }>
-      <PokemonApp />
+
+      {/* <App /> */}
+      {/* <PokemonApp /> */}
+      <TodoApp />
+      
     </Provider>
   </React.StrictMode>
 )
