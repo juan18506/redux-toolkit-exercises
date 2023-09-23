e2e7adb33a8965a03b4e9dccd3fc26931cb7d0cc
juan18506 <juan185067@gmail.com>
2023-09-23 20:07:50 -0300

    created pokemonSlice and pokemonThunks

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './App.jsx';
 import './index.css';
 import { store } from './store';
+import { PokemonApp } from './PokemonApp.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={ store }>
-      <App />
+      <PokemonApp />
     </Provider>
   </React.StrictMode>
 )
