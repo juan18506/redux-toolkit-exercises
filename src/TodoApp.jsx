38c4eb953e6eb2d88ee6304efaa3d6ea9ae19d90
juan18506 <juan185067@gmail.com>
2023-09-23 21:16:12 -0300

    added query by id

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,16 +1,49 @@
+import { useState } from 'react';
+import { useGetTodoQuery } from './store/apis/todos/todosApi';
+
 export const TodoApp = () => {
+
+  const [ todoId, setTodoId ] = useState( 1 );
+  const { data: todo, isLoading } = useGetTodoQuery( todoId );
+  console.log(todo);
+
+  const nextTodo = () => {
+    setTodoId( todoId + 1 );
+  };
+
+  const prevTodo = () => {
+    if ( todoId === 1 ) return;
+    setTodoId( todoId - 1 );
+  };
+
   return (
     <>
-      <h1>TodoApp</h1>
+      <h1>Todos -RTK Query</h1>
       <hr />
 
-      <h4>isLoading...</h4>
+      <h4>isLoading: { isLoading ? 'True' : 'False' }</h4>
 
-      <pre>...</pre>
+      <pre>{ JSON.stringify( todo ) }</pre>
 
-      <button>
+      <button onClick={ prevTodo }>
+        Prev Todo
+      </button>
+
+      <button onClick={ nextTodo }>
         Next Todo
       </button>
+
+      {/* <ul>
+        {
+          todos.map(( todo ) => (
+            <li key={ todo.id }>
+              <strong>{ todo.completed ? 'DONE' : 'Pending' }</strong>
+              { todo.title }
+            </li>
+          ))
+        }
+      </ul> */}
+
     </>
   )
 }
